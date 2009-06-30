5bed9a56e48e7b6e9bc68e5e3bb3775ee2a6b0c4
joseph.nyirenda <joseph.nyirenda@d23b51a2-b564-11dd-8f72-659bbef264a5>
2009-06-30 21:04:35 +0000

    Added Test to cover invalid cashflows

---  irr-newtonraphson-calculator/Tests/CalculatorTests.cs
+++  irr-newtonraphson-calculator/Tests/CalculatorTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using Zainco.NewtonRaphson.IRRCalculator.Domain;
+using Zainco.NewtonRaphson.IRRCalculator.Exceptions;
 
 namespace Tests
 {
@@ -13,12 +14,22 @@ namespace Tests
         [Test]
         public void TestConvergence()
         {
-            double[] cashFlows = new double[7] { -3000, 510, 131, -100, 9845, 43, 52867 };
+            var cashFlows = new double[7] { -3000, 510, 131, -100, 9845, 43, 52867 };
             var calculator = new NewtonRaphsonIRRCalculator(cashFlows);
 
             var expectedRateOfReturn = 0.77d;
             var actualRateOfReturn = calculator.Execute();
             Assert.AreEqual(expectedRateOfReturn, Math.Round(actualRateOfReturn,2), "The expected rate of return did not match");
+        }
+
+        [Test]
+        [ExpectedException(typeof(IRRCalculationException))]
+        public void Test_Should_Throw_Expected_Exception()
+        {
+            var cashFlows = new double[7] { 3000, 510, 131, -100, 9845, 43, 52867 };
+            var calculator = new NewtonRaphsonIRRCalculator(cashFlows);
+
+            calculator.Execute();
         }
     }
 }
